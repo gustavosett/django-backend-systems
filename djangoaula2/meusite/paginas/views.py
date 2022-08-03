6330b1b5d6e408c7baebe7deae4ef243f230919b
Gustavo Ribeiro <99373133+gustavosett@users.noreply.github.com>
2022-08-03 16:47:59 -0300

    Update views.py

--- djangoaula2/meusite/paginas/views.py
+++ djangoaula2/meusite/paginas/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def index(request):
-    teste = 'a cor da pedrinho q hj tem campeonato, vem dança cmg'
+    teste = 'a cor da pedrinho q hj tem campeonato, vem dança cmg, vai ver q eu te esculaxo'
     context = {
         "teste": teste
     }
