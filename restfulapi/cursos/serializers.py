670f8b911c587d29d2ab517523bfe63ac59eb3db
Gustavo Ribeiro <99373133+gustavosett@users.noreply.github.com>
2022-08-04 02:19:05 -0300

    Update serializers.py

--- restfulapi/cursos/serializers.py
+++ restfulapi/cursos/serializers.py
@@ -7,11 +7,29 @@ class AvaliacaoSerializer(serializers.ModelSerializer):
 
     class Meta:
         extra_kargs = {
-            'email' : {'write_only' : True}
+            'email': {'write_only': True}
         }
         model = Avaliacao
         fields = (
             'id',
             'curso',
-            
+            'nome',
+            'email',
+            'comentario',
+            'avaliacao',
+            'criacao',
+            'ativo'
+        )
+
+
+class CursoSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Curso
+        fields = (
+            'id',
+            'titulo',
+            'url',
+            'criacao',
+            'ativo'
         )
