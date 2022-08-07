68a7730c801d1b3cf2b93f2f2474c72d61b45e2d
Gustavo Ribeiro <99373133+gustavosett@users.noreply.github.com>
2022-08-07 06:54:15 -0300

    database

--- apidjango/api/models.py
+++ apidjango/api/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class Login(models.Model):
-    nickname = models.CharField(max_length=25)
-    email = models.EmailField(blank=True)
-    password = models.CharField(max_length=25)
+    nickname = models.CharField(max_length=25, blank=False)
+    email = models.EmailField(blank=False)
+    password = models.CharField(blank=False, max_length=120)
 
     class Meta:
         db_table = "Login"
